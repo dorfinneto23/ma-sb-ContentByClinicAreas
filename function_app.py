a79be1393458ea10e50b99b2d6146030ff61b2fd
tech dor <tech.aharoni@gmail.com>
2024-05-21 10:49:54 +0300

    first deployment

--- function_app.py
+++ function_app.py
@@ -6,5 +6,5 @@ app = func.FunctionApp()
 @app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="clinicareasconsolidation",
                                connection="medicalanalysis_SERVICEBUS") 
 def ContentByClinicAreas(azservicebus: func.ServiceBusMessage):
-    logging.info('Python ServiceBus Queue triggerr processed a message: %s',
+    logging.info('Python ServiceBuss Queue triggerr processed a message: %s',
                 azservicebus.get_body().decode('utf-8'))
