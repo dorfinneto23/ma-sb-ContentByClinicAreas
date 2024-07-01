e1c9f6345209ade7e96a57db388f892c6df0b2bf
tech dor <tech.aharoni@gmail.com>
2024-07-02 01:39:07 +0300

    update clinic area status to 1 (new)

--- function_app.py
+++ function_app.py
@@ -236,6 +236,7 @@ def Csv_Consolidation_by_clinicArea(csv_string,caseid,table_name,pagenumber):
             destinationPath = save_ContentByClinicAreas(encoded_content_csv,caseid,filename) 
             logging.info(f"fun:Csv_Consolidation_by_clinicArea step2:destinationPath: {destinationPath}")
             entity['contentCsv'] = destinationPath
+            entity['status'] = 1
             # Update the pages column
             if 'pages' in entity:
                 pages = entity['pages'].split(', ')
